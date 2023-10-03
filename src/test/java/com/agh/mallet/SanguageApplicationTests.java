ce0597fe55d3e1f9f51644dae2a9f603c641ca85
mpuczynski55 <72734576+mpuczynski55@users.noreply.github.com>
2023-10-03 23:39:00 +0200

    Initial commit

---
+++ src/test/java/com/agh/mallet/SanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.mallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
