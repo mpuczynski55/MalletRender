f0515a0cb2467b6df9925fec492a89c3da7917d2
mpuczynski55 <72734576+mpuczynski55@users.noreply.github.com>
2023-11-07 13:59:03 +0100

    GROUPS

--- mallet-core/src/main/java/com/agh/mallet/domain/user/group/contorl/UserGroupService.java
+++ mallet-core/src/main/java/com/agh/mallet/domain/user/group/contorl/UserGroupService.java
@@ -2,38 +2,38 @@ package com.agh.mallet.domain.user.group.contorl;
 
 import com.agh.api.GroupBasicDTO;
 import com.agh.mallet.domain.group.entity.GroupJPAEntity;
+import com.agh.mallet.domain.user.user.control.repository.UserRepository;
 import com.agh.mallet.domain.user.user.control.service.UserService;
-import com.agh.mallet.domain.user.user.entity.UserJPAEntity;
 import com.agh.mallet.infrastructure.mapper.GroupBasicDTOMapper;
 import com.agh.mallet.infrastructure.utils.NextChunkRebuilder;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserGroupService {
 
     private final UserService userService;
+    private final UserRepository userRepository;
     private final NextChunkRebuilder nextChunkRebuilder;
 
-    public UserGroupService(UserService userService, NextChunkRebuilder nextChunkRebuilder) {
+    public UserGroupService(UserService userService, UserRepository userRepository, NextChunkRebuilder nextChunkRebuilder) {
         this.userService = userService;
+        this.userRepository = userRepository;
         this.nextChunkRebuilder = nextChunkRebuilder;
     }
 
     public GroupBasicDTO get(int startPosition,
                              int limit,
                              String userEmail) {
-        UserJPAEntity userEntity = userService.getByEmail(userEmail);
 
-        List<GroupJPAEntity> userGroups = userEntity.getUserGroups();
+        PageRequest pageRequest = PageRequest.of(startPosition, startPosition + limit);
+        List<GroupJPAEntity> userGroups = userRepository.findAllGroupsByUserEmail(userEmail, pageRequest)
+                .getContent();
+
         String nextChunkUri = nextChunkRebuilder.rebuild(userGroups, startPosition, limit);
 
-        List<GroupJPAEntity> limitedGroups = userGroups.stream()
-                .limit(limit)
-                .collect(Collectors.toList());
-
-        return GroupBasicDTOMapper.from(limitedGroups, nextChunkUri);
+        return GroupBasicDTOMapper.from(userGroups, nextChunkUri);
     }
 }
--- mallet-core/src/main/java/com/agh/mallet/domain/user/user/control/repository/UserRepository.java
+++ mallet-core/src/main/java/com/agh/mallet/domain/user/user/control/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.agh.mallet.domain.user.user.control.repository;
 
+import com.agh.mallet.domain.group.entity.GroupJPAEntity;
 import com.agh.mallet.domain.set.entity.SetJPAEntity;
 import com.agh.mallet.domain.user.user.entity.UserJPAEntity;
 import org.springframework.data.domain.Page;
@@ -23,5 +24,8 @@ public interface UserRepository extends JpaRepository<UserJPAEntity, Long>, JpaS
 
     @Query("SELECT s FROM SetJPAEntity s WHERE s.creator.email = ?1")
     Page<SetJPAEntity> findAllSetsByUserEmail(String email, Pageable pageable);
+
+    @Query("SELECT DISTINCT g FROM GroupJPAEntity g JOIN g.contributions c JOIN c.contributor u WHERE u.email = ?1")
+    Page<GroupJPAEntity> findAllGroupsByUserEmail(String email, Pageable pageable);
 
 }
