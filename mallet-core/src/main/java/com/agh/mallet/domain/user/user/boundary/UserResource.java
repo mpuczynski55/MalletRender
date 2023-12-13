1878377312da5116f23e53160bf3b829165d2ffd
mpuczynski55 <72734576+mpuczynski55@users.noreply.github.com>
2023-12-13 20:30:40 +0100

    fixed

--- mallet-core/src/main/java/com/agh/mallet/domain/user/user/boundary/UserResource.java
+++ mallet-core/src/main/java/com/agh/mallet/domain/user/user/boundary/UserResource.java
@@ -77,7 +77,7 @@ public class UserResource {
     }
 
     @Operation(
-            summary = "Get users providing username"
+            summary = "Delete user providing username"
     )
     @GetMapping
     public ResponseEntity<List<UserDTO>> get(@RequestParam("username") String username) {
